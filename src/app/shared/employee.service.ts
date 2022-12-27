6a80c14d73a9fb381070578d67857f3ae10c0053
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-28 02:09:19 +0600

    listEmployee: Epmloyee[]=[];   listDesignation: Designation[]=[];

--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Designation, Epmloyee } from './epmloyee.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +10,6 @@ export class EmployeeService {
   constructor(myhttp:HttpClient) { }
   employeeUrl:string='https://localhost:7251/api/Employee';
   designationUrl:string='https://localhost:7251/api/Designation';
+  listEmployee: Epmloyee[]=[];
+  listDesignation: Designation[]=[];
 }
--- src/app/shared/epmloyee.model.ts
+++ src/app/shared/epmloyee.model.ts
@@ -1,4 +1,4 @@
-export class Employee {
+export class Epmloyee {
     id: number;
     name: string;
     lastName: string='';
