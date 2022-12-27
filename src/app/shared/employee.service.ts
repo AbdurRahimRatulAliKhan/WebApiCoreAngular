fecb60ce4ee663cd48906b58384622c2576a3db4
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-28 02:37:44 +0600

    post, put, get, delete

--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Designation, Epmloyee } from './epmloyee.model';
 
 @Injectable({
@@ -7,9 +8,37 @@ import { Designation, Epmloyee } from './epmloyee.model';
 })
 export class EmployeeService {
 
-  constructor(myhttp:HttpClient) { }
+  constructor(private myhttp:HttpClient) { }
   employeeUrl:string='https://localhost:7251/api/Employee';
   designationUrl:string='https://localhost:7251/api/Designation';
-  listEmployee: Epmloyee[]=[];
+  listEmployee: Epmloyee[]=[]; //object for getting data employeeList
   listDesignation: Designation[]=[];
+
+  employeeData: Epmloyee = new Epmloyee(); // object to post data / insert data
+
+  saveEmployee()
+  {
+    return this.myhttp.post(this.employeeUrl, this.employeeData);
+  }
+
+  updateEmployee()
+  {
+    return this.myhttp.put(`${this.employeeUrl}/${this.employeeData.id}`, this.employeeData);
+  }
+
+  getEmployees():Observable<Epmloyee[]>
+  {
+    return this.myhttp.get<Epmloyee[]>(this.employeeUrl);
+  }
+
+  getDesignations():Observable<Designation[]>
+  {
+    return this.myhttp.get<Designation[]>(this.designationUrl);
+  }
+
+  deleteEmployee(id:number)
+  {
+    return this.myhttp.delete(`${this.employeeUrl}/${id}`);
+  }
+
 }
