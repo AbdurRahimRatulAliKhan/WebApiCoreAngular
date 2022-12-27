1378af0fb9a0b95508e013cf536347aa9152e345
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-28 01:56:57 +0600

    myhttp and URLs

--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(myhttp:HttpClient) { }
+  employeeUrl:string='https://localhost:7251/api/Employee';
+  designationUrl:string='https://localhost:7251/api/Designation';
 }
