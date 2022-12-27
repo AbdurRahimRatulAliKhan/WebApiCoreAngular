384012fc0dc02723ffb09aa5edb93ea8afceb2a6
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-28 03:40:30 +0600

    getting empService.listEmployee data

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../shared/employee.service';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent {
+// export class EmployeeDetailsComponent {
 
+// }
+
+export class EmployeeDetailsComponent implements OnInit {
+  constructor(public empService:EmployeeService){}
+  ngOnInit(): void {
+    this.empService.getEmployees().subscribe(data=>{
+      this.empService.listEmployee=data;
+    });
+  }
 }
